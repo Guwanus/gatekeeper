5dcc668cda7eed0f82e76d264043dee70aeac67b
juan <juanmanuel.hol@student.fontys.nl>
2025-02-13 11:27:55 +0100

    GatekeeperV3 af

--- gatekeeper.py
+++ gatekeeper.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 
 uur = datetime.now().hour
+kenteken = ["71-pd-dd", "29-jkf-5", "87-pb=hr", "65-hkr-8"]
+invoerkenteken = input("Voer kenteken in:")
 
-if uur >= 7 and uur <= 12:
-    print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
-elif uur >= 12 and uur <= 18:
-    print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
-elif uur >= 18 and uur <= 23:
-    print("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+if invoerkenteken in kenteken:
+
+    if uur >= 7 and uur <= 12:
+        print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+    elif uur >= 12 and uur <= 18:
+        print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+    elif uur >= 18 and uur <= 23:
+        print("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+    else:
+        print("Sorry, de parkeerplaats is ’s nachts gesloten")
+    
 else:
-    print("Sorry, de parkeerplaats is ’s nachts gesloten")
+    print("U heeft helaas geen toegang tot het parkeerterrein")
