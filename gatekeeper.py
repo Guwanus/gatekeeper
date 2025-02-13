81cf234766e97a87059da567fcc641df0a398d8c
juan <juanmanuel.hol@student.fontys.nl>
2025-02-13 10:57:05 +0100

    GatekeeperV2 done

--- gatekeeper.py
+++ gatekeeper.py
@@ -1,1 +1,13 @@
-print("Welkom bij Fonteyn Vakantieparken")
+from datetime import datetime
+
+def begroeting():
+    uur = datetime.now().hour
+
+    if 7 <= uur < 12:
+        print("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+    elif 12 <= uur < 18:
+        print("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+    elif 18 <= uur < 23:
+        print("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+    else:
+        print("Sorry, de parkeerplaats is ’s nachts gesloten")
